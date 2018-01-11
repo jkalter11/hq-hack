369692ec298fa2eeca9cf6ff40990be46610595a
robert <robert.otherone@gmail.com>
2018-01-10 22:20:52 -0500

    Parsed string into workable variable

--- start-script.sh
+++ start-script.sh
@@ -1,8 +1,19 @@
-#! /bin/bash
+#!/bin/bash
 
 #gnome-screenshot -f ./temp.png
-#convert ./temp.png -crop 600x450+2700+350 ./output.png
+#convert ./temp.png -crop 600x450+2700+150 ./output.png
 
-
+#Generate text from question
 tesseract output.png output
 
+tr '\n' ' ' < output.txt > question.txt
+
+
+value=$(<question.txt)
+
+query=${value%?*}
+
+echo "$query"
+
+#Search on Google
+#firefox google.com/search?q=$query
